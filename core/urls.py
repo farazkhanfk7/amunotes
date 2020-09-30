1ac17129e63f005408b9b123504dcf3c9680385d
farazkhanfk7 <farazkhan138@gmail.com>
2020-10-01 00:09:07 +0530

    updated orm

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-from .views import FacultyListView, FacultyView
 urlpatterns = [
     path('',views.home,name="home"),
-    path('facultylist',FacultyListView.as_view(),name="facultylist"),
-    path('faculty/<slug>/',FacultyView.as_view(), name="faculty")
+    path('facultylist',views.facultylist,name="facultylist"),
+    path('faculty/<slug>/',views.faculty, name="faculty")
 ]
